e9a12983819154216cb0aaaf7bc617db952cae70
xtYao <xiaotong.yao@nyu.edu>
2021-04-28 11:32:10 -0400

    prepare to merge PRs

--- wgs.run.R
+++ wgs.run.R
@@ -6,11 +6,13 @@ if (!exists("opt")){
         make_option(c("--outdir"), type = "character", default = './', help = "Directory to dump output into"),
         make_option(c("--pair"), type = "character", help = "ID of this sample"),
         make_option(c("--jabba_rds"), type = "character", help = "jabba output, 'jabba.simple.rds'"),
+        make_option(c("--cbs_cov_rds"), type = "character", default = NA_character_, help = "cbs_cov output"),
+        make_option(c("--het_pileups_wgs"), type = "character", default = NA_character_, help = "cbs_cov output"),
         make_option(c("--complex"), type = "character", help = "complex event caller, RDS"),
         make_option(c("--fusions"), type = "character", help = "fusions module output, RDS"),
         make_option(c("--proximity"), type = "character", help = "proximity module output, RDS"),
         make_option(c("--gencode"), type = "character", default = "~/DB/GENCODE/hg19/gencode.v19.annotation.gtf", help = "GENCODE gene models in GTF/GFF3 formats"),
-        make_option(c("--chrom_sizes"), type = "character", default = "/gpfs/commons/groups/imielinski_lab/DB/UCSC/hg19.broad.chrom.sizes", help = "chrom.sizes file of the reference genome"),
+        make_option(c("--chrom_sizes"), type = "character", default = "~/DB/UCSC/hg19.broad.chrom.sizes", help = "chrom.sizes file of the reference genome"),
         make_option(c("--knit_only"), type = "logical", default = FALSE, action = "store_true", help = "if true, skip module and just knit"),
         make_option(c("--amp_thresh"), type = "numeric", default = 4,
                     help = "Threshold over ploidy to call amplification"),
@@ -19,7 +21,6 @@ if (!exists("opt")){
         make_option(c("--server"), type = "character", default = "https://mskilab.com/gGraph/", help = "URL of the gGnome.js browser"),
         make_option(c("--tumor_type"), type = "character", default = "", help = "tumor type"),
         make_option(c("--overwrite"), type = "logical", default = FALSE, action = "store_true", help = "overwrite existing data in the output dir")
-        ## make_option(c("--chr"), type = "logical", default = FALSE, action = "store_true", help = "Force the reference genome to have chr prefix"),
     )
     parseobj = OptionParser(option_list = option_list)
     opt = parse_args(parseobj)
@@ -61,14 +62,27 @@ if (!opt$knit_only){
         saveRDS(gg, paste0(opt$outdir, "/complex.rds"))
     }
 
-    message("Prepare coverage plots")
+    message("Prepare coverage data")
     if (!file.exists(paste0(opt$outdir, "/coverage.gtrack.rds"))){
         cvgt = covcbs(opt$cbs_cov_rds, purity = jabba$purity, ploidy = jabba$ploidy, rebin = 5e3)
         saveRDS(cvgt, paste0(opt$outdir, "/coverage.gtrack.rds"))
     } else {
         cvgt = readRDS(paste0(opt$outdir, "/coverage.gtrack.rds"))
     }
-    
+
+    if (!file.exists(paste0(opt$outdir, "/hets.gtrack.rds"))){
+        hgt = covcbs(opt$cbs_cov_rds, purity = jabba$purity, ploidy = jabba$ploidy, rebin = 5e3)
+        saveRDS(cvgt, paste0(opt$outdir, "/coverage.gtrack.rds"))
+    } else {
+        cvgt = readRDS(paste0(opt$outdir, "/coverage.gtrack.rds"))
+    }
+
+    message("Generate full genome gTrack plot")
+    gts = c(cvgt, gg$gtrack(name = opt$pair, height = 30))
+
+    png(filename = paste0(opt$outdir, "/genome.wide.gtrack.png"), width = 2700, height = 900)
+    plot(gts, hg, gap = 1e7, y0 = 0, cex.label = 2, yaxis.cex = 0.5)
+    dev.off()
 }
 
 message("Start knitting")
