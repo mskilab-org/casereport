8bac12de50b8c1bacc728b99956340d7db9cc79e
ShaiberAlon <alon.shaiber@gmail.com>
2021-04-30 15:04:27 -0400

    added driver genes cnv table

--- wgs.run.R
+++ wgs.run.R
@@ -59,37 +59,71 @@ if (!opt$knit_only) {
 
     message("Returning Purity, Ploidy, and run 'events' if not already provided")
     jabba = readRDS(opt$jabba_rds)
-    if (file.exists(opt$complex) & file.size(opt$complex)>0){
-        file.copy(opt$complex, paste0(opt$outdir, "/complex.rds"))
-        gg = readRDS(opt$complex)
+    gg_fn = paste0(opt$outdir, "/complex.rds")
+    if (check_file(gg_fn, overwrite = opt$overwrite)){
+        gg = readRDS(gg_fn)
     } else {
-        gg = events(gG(jabba = jabba))
-        saveRDS(gg, paste0(opt$outdir, "/complex.rds"))
+        if (file.exists(opt$complex) & file.size(opt$complex)>0){
+            file.copy(opt$complex, paste0(opt$outdir, "/complex.rds"))
+            gg = readRDS(opt$complex)
+        } else {
+            gg = events(gG(jabba = jabba))
+            saveRDS(gg, paste0(opt$outdir, "/complex.rds"))
+        }
+    }
+
+    message('Calling CNVs for oncogenes and tumor suppressor genes')
+    # get the ncn data from jabba
+    genes_cn.fn = paste0(opt$outdir, '/genes_cn.rds')
+    oncogenes.fn = file.path(opt$libdir, "data", "onc.rds")
+    tsg.fn = file.path(opt$libdir, "data", "tsg.rds")
+    if (check_file(genes_cn.fn, overwrite = opt$overwrite)){
+        genes_cn = readRDS(genes_cn.fn)
+    } else {
+        kag_rds = gsub("jabba.simple.rds", "karyograph.rds", opt$jabba_rds)
+        nseg = NULL
+        if (file.exists(kag_rds) & file.size(kag_rds) > 0){
+            message('Loading JaBbA karyograph from ', kag_rds)
+            kag = readRDS(kag_rds)
+            if ('ncn' %in% names(mcols(kag$segstats))){
+                nseg = kag$segstats[,c('ncn')]
+            } else {
+                message('JaBbA karyograph does not contain the "ncn" field so CN = 2 will be assumed for the normal copy number of all seqnames.')
+            }
+        } else {
+            # TODO: perhaps we should allow to directly provide a nseg input
+            message('JaBbA karyograph was not found at the expected location (', kag_rds, ') so we will use CN = 2 for the normal copy number of all chromosomes.')
+        }
+
+        genes_cn = get_gene_copy_numbers(gg, pge = pge, nseg = nseg)
+        genes_cn_annotated = get_gene_ampdel_annotations(genes_cn, amp.thresh = opt$amp_thresh,
+                                       del.thresh = opt$del_thresh)
+
+        saveRDS(genes_cn_annotated, genes_cn.fn)
+    }
+
+    driver.genes.cnv.fn = paste0(opt$outdir, '/driver.genes.cnv.txt')
+    if (!check_file(driver.genes.cnv.fn, overwrite = opt$overwrite)){
+        if (genes_cn[, .N] > 0){
+            onc = readRDS(oncogenes.fn)
+            tsg = readRDS(tsg.fn)
+            driver.genes_cn = genes_cn_annotated[gene_name %in% c(onc, tsg)]
+            fields = c("gene_name", "cnv", "min_cn", "max_cn", "min_normalized_cn", "max_normalized_cn", "number_of_cn_segments", "ncn", "seqnames", "start", "end", "width", "gene_id", "gene_type", "source",  "level", "hgnc_id", "havana_gene")
+            fields = intersect(fields, names(driver.genes_cn))
+            fwrite(driver.genes_cn[, ..fields], driver.genes.cnv.fn)
+        }
     }
 
     message("Prepare coverage data")
-    if (!file.exists(paste0(opt$outdir, "/coverage.gtrack.rds"))){
+    cvgt_fn = paste0(opt$outdir, "/coverage.gtrack.rds")
+    if (check_file(cvgt_fn, overwrite = opt$overwrite)){
+        cvgt = readRDS(cvgt_fn)
+    } else {
         ## pull coverage file from jabba_rds
         cov.file = readRDS(file.path(dirname(opt$jabba_rds), "cmd.args.rds"))$coverage
         cvgt = covcbs(cov.file, purity = jabba$purity, ploidy = jabba$ploidy, rebin = 5e3)
-        saveRDS(cvgt, paste0(opt$outdir, "/coverage.gtrack.rds"))
-    } else {
-        cvgt = readRDS(paste0(opt$outdir, "/coverage.gtrack.rds"))
+        saveRDS(cvgt, cvgt_fn)
     }
-
-    ## xtYao legacy code
-    ## if (!file.exists(paste0(opt$outdir, "/hets.gtrack.rds"))){
-    ##     hgt = covcbs(opt$het_pileups_wgs,
-    ##                  purity = jabba$purity, ploidy = jabba$ploidy, rebin = 5e3)
-    ##     saveRDS(cvgt, paste0(opt$outdir, "/coverage.gtrack.rds"))
-    ## } else {
-    ##     cvgt = readRDS(paste0(opt$outdir, "/coverage.gtrack.rds"))
-    ## }
-    ## message("Generate full genome gTrack plot")
-    ## gts = c(cvgt, gg$gtrack(name = opt$pair, height = 30))
-    ## png(filename = paste0(opt$outdir, "/genome.wide.gtrack.png"), width = 2700, height = 900)
-    ## plot(gts, hg, gap = 1e7, y0 = 0, cex.label = 2, yaxis.cex = 0.5)
-    ## dev.off()
 
     wgs.gtrack.fname = file.path(opt$outdir, "wgs.gtrack.png")
     wgs.circos.fname = file.path(opt$outdir, "wgs.circos.png")
