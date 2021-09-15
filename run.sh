630d7845c6b1cbd9b01acc8524ec9f81014e6ccd
Zi-Ning Choo <zining01@gmail.com>
2021-09-15 16:23:34 -0400

    add step for optimizing PNG

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ source ~/.bash_profile
 module unload gcc
 module load gcc/9.2.0
 
+module load anaconda3
+source /nfs/sw/anaconda3/anaconda3-10.19/etc/profile.d/conda.sh
+conda activate casereport
+
 Rscript $@
