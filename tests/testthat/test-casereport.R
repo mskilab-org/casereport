f249e2f231c85f37f139b4870e9af68c65aae549
ShaiberAlon <alon.shaiber@gmail.com>
2022-02-04 11:41:51 -0500

    starting to add tests: added tests directory and added a mock dataset that is based on CCLE data: G32831

---
+++ tests/testthat/test-casereport.R
@@ -0,0 +1,13 @@
+test_that("Run case report for G32831", {
+    gdt = data.table(
+             pair = 'G32831',
+             jabba_rds = system.file('extdata', 'G32831/jabba.simple.rds', package = "casereport"),
+             cbs_cov_rds = system.file('extdata', 'G32831/G32831.Detroit_562.chr11.window.rds', package = "casereport"),
+             fusions = system.file('extdata', 'G32831/fusions.rds', package = "casereport"),
+             complex = system.file('extdata', 'G32831/complex.rds', package = "casereport"),
+             het_pileups_wgs = system.file('extdata', 'G32831/ G32831.Detroit_562.chr11.window.hets.txt', package = "casereport"),
+             deconstruct_sigs = system.file('extdata', 'G32831/deconstructSigs.out.rds', package = "casereport"),
+             deconstruct_variants =  system.file('extdata', 'G32831/variants.out.txt.gz', package = "casereport"),
+             snv_vcf = system.file('extdata', 'G32831/G32831.Detroit_562.vcf', package = "casereport"),
+             snpeff_snv_bcf = system.file('extdata', 'G32831/annotated.bcf', package = "casereport"))
+})
