0d435119f4775bd9f71e56a2208aae5f449f0a7d
ShaiberAlon <alon.shaiber@gmail.com>
2022-02-04 11:45:01 -0500

    fixy for test data

--- tests/testthat/test-casereport.R
+++ tests/testthat/test-casereport.R
@@ -5,7 +5,7 @@ test_that("Run case report for G32831", {
              cbs_cov_rds = system.file('extdata', 'G32831/G32831.Detroit_562.chr11.window.rds', package = "casereport"),
              fusions = system.file('extdata', 'G32831/fusions.rds', package = "casereport"),
              complex = system.file('extdata', 'G32831/complex.rds', package = "casereport"),
-             het_pileups_wgs = system.file('extdata', 'G32831/ G32831.Detroit_562.chr11.window.hets.txt', package = "casereport"),
+             het_pileups_wgs = system.file('extdata', 'G32831/G32831.Detroit_562.chr11.window.hets.txt', package = "casereport"),
              deconstruct_sigs = system.file('extdata', 'G32831/deconstructSigs.out.rds', package = "casereport"),
              deconstruct_variants =  system.file('extdata', 'G32831/variants.out.txt.gz', package = "casereport"),
              snv_vcf = system.file('extdata', 'G32831/G32831.Detroit_562.vcf', package = "casereport"),
