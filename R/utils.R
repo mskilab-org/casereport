159924581c0e7349aadb934a74698c96b9b56c9c
Juan Sebastián Andrade <mr.jekyll31no@gmail.com>
2022-02-09 19:09:07 -0500

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1311,16 +1311,15 @@ deconstructsigs_histogram = function(sigs.fn = NULL,
 	
     fwrite(allsig[pair== id,],file.path(outdir,"Sig.csv"))
     
-    allsig$Signature=as.character(allsig$Signature)
     thisMet=sigMet[sigMet$Signature %in% allsig$Signature,]
     thisMet=thisMet[, Signature := factor(Signature, levels = new.slevels)]
     
     
     allsig=merge(allsig,thisMet,by='Signature')
-    allsig$Signature_Description=paste(allsig$MP.Summary,"\n (",as.character(allsig$Signature),")")
+    allsig$Signature_Description=paste0(allsig$MP.Summary,"\n (",as.character(allsig$Signature),")")
     allsig$Signature_Description=str_replace(allsig$Signature_Description,"@","\n")
     allsig$Signature_Description=str_replace(allsig$Signature_Description,"@temozolomide","\ntemozolomide")
-    #allsig=allsig[order(-allsig$sig_count),]
+    allsig=allsig[order(pair,-allsig$sig_count),]
     fwrite(allsig,"/gpfs/commons/home/jandrademartínez/allsig.txt")
 	
     sigbar = ggplot(allsig, aes(y = Signature_Description, x = sig_count, fill = Signature)) +
