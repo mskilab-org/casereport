e3c9f89c0f689126a56af437521f736787a01b71
Juan Sebastián Andrade <mr.jekyll31no@gmail.com>
2022-03-13 19:53:44 -0400

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -3506,6 +3506,7 @@ makeSummaryTable = function(cnv_table,fusions_table,expression_table,mutations_t
     summaryTable=summaryTable[!(summaryTable$type=="del" & summaryTable$role=="ONC"),]
     summaryTable=summaryTable[!(grepl("underexpression",summaryTable$type,fixed=TRUE) & summaryTable$role=="ONC"),]
     summaryTable=summaryTable[!(grepl("overexpression",summaryTable$type,fixed=TRUE) & summaryTable$role=="TSG"),]
+    summaryTable=summaryTable[!is.na(summaryTable$type) & summaryTable$type!="",]
 
     summaryTable$type=str_replace_all(summaryTable$type,"del"," loss")
   
