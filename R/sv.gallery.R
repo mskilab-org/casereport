7a92ab931805b95c27f77bd1386f9c10181020cb
Juan Sebastián Andrade <mr.jekyll31no@gmail.com>
2022-05-23 03:43:28 -0400

    Add files via upload

--- R/sv.gallery.R
+++ R/sv.gallery.R
@@ -304,6 +304,8 @@ cn.plot = function(drivers.fname = NULL,
     }
 
     message(nrow(drivers.dt))
+    message(gg.rds)
+
     ## if empty data table, return
     if (nrow(drivers.dt) > 0) {
 
@@ -409,15 +411,21 @@ cn.plot = function(drivers.fname = NULL,
         }
         drivers.gr$win = win.gr.other$win ## copy over plot window possibly return vector in the future
         ## make one plot per range in drivers gr
+      
         print(drivers.dt)
         pts = lapply(1:length(drivers.gr),
                      function(ix) {
 
                          if (!file.exists(drivers.dt$plot.fname[ix]) | overwrite){
                              ## prepare window
-                             if("cnv" %in% colnames(drivers.dt) & (grepl("homdel",drivers.dt$cnv[ix]) | grepl("hetdel",drivers.dt$cnv[ix]))){
+                           windowDef=FALSE
+                           if("cnv" %in% colnames(drivers.dt)){
+                              if(grepl("homdel",drivers.dt$cnv[ix]) | grepl("hetdel",drivers.dt$cnv[ix])){
                                 win=win.gr[[ix]]
-                            }else{
+                                windowDef=TRUE
+                            }
+                             }
+                           if(!windowDef){
                                 if (is.null(drivers.gr$win) || is.na(drivers.gr$win[[ix]])) {
                                     win = drivers.gr[ix]
                                 } else {
